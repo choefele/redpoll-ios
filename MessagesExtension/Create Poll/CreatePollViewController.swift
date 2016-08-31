5429da75f8fcec5e8f46f7852a132c86df30dfda
Claus Höfele <claus@claushoefele.com>
2016-08-31 20:48:04 +0200

    Add simple form

--- MessagesExtension/Create Poll/CreatePollViewController.swift
+++ MessagesExtension/Create Poll/CreatePollViewController.swift
@@ -7,7 +7,28 @@
 //
 
 import UIKit
+import Eureka
 
-class CreatePollViewController: UITableViewController {
-    
+class CreatePollViewController: FormViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let section0 = Section("Section1")
+        form.append(section0)
+        section0.append(TextRow() { row in
+            row.title = "Text Row"
+            row.placeholder = "Enter text here"
+        })
+        section0.append(PhoneRow() {
+            $0.title = "Phone Row"
+            $0.placeholder = "And numbers here"
+        })
+        
+        let section1 = Section("Section1")
+        form.append(section1)
+        section1.append(DateRow() {
+            $0.title = "Date Row"
+            $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        })
+    }
 }
