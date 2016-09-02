2e99e731c53a6ba06db924ff954b706277b87a10
Claus Höfele <claus.hoefele@here.com>
2016-09-02 17:58:48 +0200

    Simple pie chart without view in Storyboard

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -11,9 +11,6 @@ import Messages
 import Charts
 
 class MessagesViewController: MSMessagesAppViewController {
-
-    @IBOutlet weak var pieChartView: PieChartView!
-
     override func willBecomeActive(with conversation: MSConversation) {
         super.willBecomeActive(with: conversation)
         
@@ -53,25 +50,67 @@ class MessagesViewController: MSMessagesAppViewController {
             child.removeFromParentViewController()
         }
         
-//        // Embed the new controller.
-//        addChildViewController(controller)
-//        
-//        controller.view.frame = view.bounds
-//        controller.view.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(controller.view)
-//        
-//        controller.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-//        controller.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-//        controller.view.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-//        controller.view.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
-//        
-//        controller.didMove(toParentViewController: self)
+        // Embed the new controller.
+        addChildViewController(controller)
+        
+        controller.view.frame = view.bounds
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(controller.view)
+        
+        controller.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        controller.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        controller.view.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        controller.view.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
+        
+        controller.didMove(toParentViewController: self)
+
+        // Pie
+        let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let pieChartView = PieChartView(frame: rect)
 
         let yVals = [BarChartDataEntry(value: 1, xIndex: 0), BarChartDataEntry(value: 2, xIndex: 1), BarChartDataEntry(value: 3, xIndex: 3)]
         let dataSet = PieChartDataSet(yVals: yVals, label: "Label")
+        dataSet.colors = ChartColorTemplates.pastel()
         let xVals = ["1", "2", "3"]
         let data = PieChartData(xVals: xVals, dataSet: dataSet)
         pieChartView.data = data
+        pieChartView.holeColor = UIColor.clear
+        pieChartView.drawSliceTextEnabled = false
+        pieChartView.legend.enabled = false
+
+        UIGraphicsBeginImageContextWithOptions(pieChartView.bounds.size, true, UIScreen.main.scale)
+        defer { UIGraphicsEndImageContext() }
+
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(UIColor.red.cgColor)
+            context.fill(pieChartView.bounds)
+            pieChartView.layer.render(in: context)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            print()
+        }
+
+//        NSUIGraphicsBeginImageContextWithOptions(size: bounds.size, isOpaque || !transparent, NSUIMainScreen()?.nsuiScale ?? 1.0)
+//        defer { NSUIGraphicsEndImageContext() }
+//
+//        if let context = NSUIGraphicsGetCurrentContext() {
+//            let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: bounds.size)
+//
+//            if (isOpaque || !transparent)
+//            {
+//                // Background color may be partially transparent, we must fill with white if we want to output an opaque image
+//                context.setFillColor(NSUIColor.white.cgColor)
+//                context.fill(rect)
+//
+//                if (self.backgroundColor !== nil)
+//                {
+//                    context.setFillColor((self.backgroundColor?.cgColor)!)
+//                    context.fill(rect)
+//                }
+//            }
+//            
+//            nsuiLayer?.render(in: context)
+//            let image = NSUIGraphicsGetImageFromCurrentImageContext()
+
     }
     
     private func instantiateViewController<ViewController>() -> ViewController {
