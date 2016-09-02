a1b52aef4d1c2e0051982046a211b2218c132304
Claus Höfele <claus@claushoefele.com>
2016-09-02 21:21:15 +0200

    Simplify rendering pie chart

--- MessagesExtension/MessagesViewController+PollViewControllerDelegate.swift
+++ MessagesExtension/MessagesViewController+PollViewControllerDelegate.swift
@@ -36,14 +36,39 @@ extension MessagesViewController: PollViewControllerDelegate {
         //        message.url = components.url!
 
         let layout = MSMessageTemplateLayout()
-        //        layout.image = iceCream.renderSticker(opaque: true)
+        layout.image = renderPollForm(pollForm: pollForm)
         layout.caption = caption
         message.layout = layout
 
         return message
     }
 
-    private func renderPollForm(pollForm: PollForm) {
+    private func renderPollForm(pollForm: PollForm) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 300, height: 300)
+        let pieChartView = PieChartView(frame: rect)
+        pieChartView.holeColor = UIColor.clear
+        pieChartView.transparentCircleRadiusPercent = 0
+        pieChartView.drawSliceTextEnabled = false
+        pieChartView.legend.enabled = false // needs to happen before setting data
         
+        let yVals = [BarChartDataEntry(value: 1, xIndex: 0), BarChartDataEntry(value: 2, xIndex: 1), BarChartDataEntry(value: 3, xIndex: 3)]
+        let dataSet = PieChartDataSet(yVals: yVals, label: "Label")
+        dataSet.colors = ChartColorTemplates.pastel()
+        dataSet.selectionShift = 0
+        let xVals = ["1", "2", "3"]
+        let data = PieChartData(xVals: xVals, dataSet: dataSet)
+        pieChartView.data = data
+        
+        let viewPortHandler = ChartViewPortHandler(width: rect.width, height: rect.height)
+        let pieChartRenderer = PieChartRenderer(chart: pieChartView, animator: ChartAnimator(), viewPortHandler: viewPortHandler)
+        
+        let renderer = UIGraphicsImageRenderer(bounds: rect)
+        let image = renderer.image { context in
+            pieChartRenderer.drawData(context: context.cgContext)
+            pieChartRenderer.drawExtras(context: context.cgContext)
+            pieChartRenderer.drawValues(context: context.cgContext)
+        }
+        
+        return image
     }
 }
